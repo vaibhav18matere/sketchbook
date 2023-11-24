2ae9874864900abb2d6deba5d9d9299a9854b7ee
Vaibhav Matere <vpm00718897@gmail.com>
2023-11-24 18:12:56 +0530

    reference for active size and color

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,11 @@
 import { useRef, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+
 const Board = () => {
      const canvasRef = useRef(null);
-
+     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+     const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
+     // console.log(color, size);
      useEffect(() => {
           
           if (!canvasRef.current) return
