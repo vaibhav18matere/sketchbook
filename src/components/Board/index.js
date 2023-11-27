4384cc1fe13412b33d4ac77d66a3a69eca932e0f
Vaibhav Matere <vpm00718897@gmail.com>
2023-11-28 00:27:04 +0530

    feat : added painiting on canvas functionality

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,22 +1,63 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useLayoutEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 
 const Board = () => {
      const canvasRef = useRef(null);
+     const shouldDraw = useRef(false);
      const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
      const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
      // console.log(color, size);
-     useEffect(() => {
-          
+     // before browser mounting on page
+     useLayoutEffect(() => {
+
           if (!canvasRef.current) return
           const canvas = canvasRef.current
           const context = canvas.getContext('2d');
-          
-          // when mounting
           canvas.width = window.innerWidth;
-          canvas.height = window.innerheight;
+          canvas.height = window.innerHeight;
+
+          const handleMouseDown = (e) => {
+               shouldDraw.current = true;
+               context.beginPath();
+               context.moveTo(e.clientX, e.clientY);
+          };
+          const handleMouseMove = (e) => {
+               if (!shouldDraw.current) return
+               context.lineTo(e.clientX, e.clientY);
+               context.stroke();
+          };
+          const handleMouseUp = () => {
+               shouldDraw.current = false;
+
+          };
+
+          canvas.addEventListener('mousedown', handleMouseDown);
+          canvas.addEventListener('mousemove', handleMouseMove);
+          canvas.addEventListener('mouseup', handleMouseUp);
+
+          // cleanup
+          return () => {
+               canvas.removeEventListener('mousedown', handleMouseDown);
+               canvas.removeEventListener('mousemove', handleMouseMove);
+               canvas.removeEventListener('mouseup', handleMouseUp);
+          }
+
      }, []);
-     
+
+
+     // when painting on screen
+     useEffect(() => {
+          if (!canvasRef.current) return
+          const canvas = canvasRef.current;
+          const context = canvas.getContext('2d');
+
+          const changeConfig = () => {
+               context.strokeStyle = color;
+               context.lineWidth = size;
+          }
+          changeConfig();
+
+     }, [color, size])
 
      return (
           <>
